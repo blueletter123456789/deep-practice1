680ff762a1c6b9703b44f39033b996892f4aeceb
Shuhei Abe <blueletter123456789@gmail.com>
2022-04-12 21:08:43 +0900

    create nor gate

--- ch02/nor_gate.py
+++ ch02/nor_gate.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def OR(x1, x2):
+def NOR(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.3
+    w = np.array([-0.5, -0.5])
+    b = 0.3
     tmp = np.sum(x*w)+b
     if tmp <= 0:
         return 0
@@ -13,4 +13,4 @@ def OR(x1, x2):
 if __name__ == '__main__':
     x = [(0, 0), (1, 0), (0, 1), (1, 1)]
     for x1, x2 in x:
-        print(OR(x1, x2))
+        print(NOR(x1, x2))
