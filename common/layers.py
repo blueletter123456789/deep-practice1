8abf3952271409b136157535a5702654b0362b4f
Shuhei Abe <blueletter123456789@gmail.com>
2022-09-10 09:21:40 +0900

    create sigmoid layer

--- common/layers.py
+++ common/layers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Relu:
     def __init__(self):
         """initialize RELU class
@@ -33,5 +35,36 @@ class Relu:
         dout[self.mask] = 0
 
         dx = dout
+
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+    
+    def forward(self, x):
+        """Forward in a Sigmoid layer
+
+        Args:
+            x (ndarray): Value of x
+
+        Returns:
+            ndarray: Result of sigmoid
+        """
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+    
+    def backward(self, dout):
+        """Backward in a Sigmoid layer
+
+        Args:
+            dout (ndarray): Differentiation of ndarray type
+
+        Returns:
+            ndarray: Differentiation of ndarray type
+        """
+        dx = dout * (1 - dout) * dout
 
         return dx
